a3af586be84b556a32d6d2d5aef8820ffe35ba85
Christopher Woods <Christopher.Woods@bristol.ac.uk>
2021-12-10 16:47:34 +0000

    Reproduces the perl code and is now substantially faster :-)

--- select_minimal_markers.py
+++ select_minimal_markers.py
@@ -15,20 +15,20 @@ from pandas.core import indexing
 # and the rest ignored. You will get a warning if this limit is reached.
 # This feature is intended to speed up the runtime for really big datasets
 # such as 800K Axiom genotyping.
-max_markers = 1000000000000
+max_markers: int = 1000000000000
 
 # Ignore markers with less than this proportion of valid (0, 1 of 2 ) calls.
 # (Bad calls might be encoded as -1 or NaN)
-min_call_rate = 0.9
+min_call_rate: float = 0.9
 
 # MAF is minor allele frequency.  This is set to a low level to include as
 # many markers as possible but exclude rare error calls.
 # It probably needs optimising for your data.
-min_maf = 0.001
+min_maf: float = 0.001
 
 # Get the filename from the command line
 try:
-    input_file = sys.argv[1]
+    input_file: str = sys.argv[1]
 except Exception:
     print("USAGE: python select_minimal_markers.py genotypes.csv")
     sys.exit(0)
@@ -37,21 +37,21 @@ except Exception:
 # easily add a test to change to tab separated if needed
 df = pd.read_csv(input_file, index_col="code")
 
-# get the number of columns - this should be the same for every row
-ncols = df.shape[1]
-nrows = df.shape[0]
-
 # Common formats are A, B for homozygotes (AA and BB) and AB for
 # heterozygotes or 0,1,2 for hom AA, het AB and hom BB.
 # We're going to convert A AB B to 0,1,2 format from here on
 df = df.replace("AB", 1).replace("A", 0).replace("B", 2)
 
 
-def clean(x):
+def clean(x: any) -> int:
+    """Quick cleaning function that returns 0, 1, 2 if x is
+       0, 1 or 2, or returns -1 if it is any other value
+    """
+
     try:
-        x = int(x)
-        if x >= 0 and x <= 2:
-            return x
+        xi: int = int(x)
+        if xi >= 0 and xi <= 2:
+            return xi
     except Exception:
         pass
 
@@ -67,7 +67,7 @@ df = df.applymap(
 )
 
 
-def get_pattern_from_array(array):
+def get_pattern_from_array(array) -> str:
     """Return a pattern encoded as an integer array
        into a string - this converts values less than
        0 into x
@@ -79,24 +79,26 @@ def get_pattern_from_array(array):
 pattern_to_idx = {}
 
 # process each row
+nrows: int = df.shape[0]
+
 for i in range(0, nrows):
     alleles = {}
-    fails = 0
+    fails: int = 0
 
     row = df.iloc[i]
     counts = row.value_counts()
 
     for key in counts.keys():
         if key == -1:
-            fails = int(counts[-1])
+            fails: int = int(counts[-1])
         else:
             alleles[key] = int(counts[key])
 
-    n_alleles = len(alleles.keys())
+    n_alleles: int = len(alleles.keys())
 
-    rowlen = len(row)
+    rowlen: int = len(row)
 
-    call_rate = float(rowlen - fails) / rowlen
+    call_rate: float = float(rowlen - fails) / rowlen
 
     if n_alleles > 1 and call_rate > min_call_rate:
         # turn the data into a single string that contains the pattern
@@ -109,9 +111,7 @@ for i in range(0, nrows):
 
         pattern_to_idx[pattern] = idx
 
-n = len(pattern_to_idx)
-
-print(f"Loaded marker data for {n} distinct patterns")
+print(f"Loaded marker data for {len(pattern_to_idx)} distinct patterns")
 
 # All done with reading data from the SNP input file now.
 
@@ -128,30 +128,29 @@ order_by_maf = {}
 
 for pattern in keys:
     idx = pattern_to_idx[pattern]
-    zero = pattern.count("0")
-    one = pattern.count("1")
-    two = pattern.count("2")
+    zero: int = pattern.count("0")
+    one: int = pattern.count("1")
+    two: int = pattern.count("2")
 
     # Logic steps to work out which is the second most common
     # call, which we'll define as the minor allele.
     if one >= zero and zero >= two:
-        minor = zero
+        minor: int = zero
     elif zero >= one and one >= two:
-        minor = one
+        minor: int = one
     elif zero >= two and two >= one:
-        minor = two
+        minor: int = two
     elif one >= two and two >= zero:
-        minor = two
+        minor: int = two
     elif two >= one and one >= zero:
-        minor = one
+        minor: int = one
     elif two >= zero and zero >= one:
-        minor = zero
+        minor: int = zero
     else:
         print(f"WARNING: MISSED CONDITION!")
         sys.exit(-1)
 
-    maf = float(minor) / ncols
-    # print(f"{idx}\t{maf}\t{zero}\t{one}\t{two}\n")
+    maf: float = float(minor) / len(pattern)
 
     if maf > min_maf:
         pattern_to_maf[pattern] = maf
@@ -180,132 +179,120 @@ for score in scores:
 # now sort the selected markers by pattern
 selected.sort()
 
+# copy the patterns into a numpy array
+patterns = np.zeros((len(selected), len(selected[0])), np.int8)
 
-@numba.jit(nopython=True, fastmath=True, nogil=True)
-def score_pattern(pattern, matrix):
-    n = len(pattern)
+for i in range(0, len(selected)):
+    pattern = selected[i]
 
-    # The logic here is to loop through the genotype string for this
-    # marker row and compare each position with every position to
-    # the right of it
-    # This will fill out one triangular half of the matrix so we end
-    # up comparing col1 with col2 then col3 but we don't waste time
-    # going back and comparing col2 to col1.
-    score = 0
+    for j in range(0, len(pattern)):
+        x = pattern[j]
 
-    for i in numba.prange(0, n):
-        ichar = pattern[i]
-
-        if ichar != "x":
-            for j in range(i+1, n):
-                jchar = pattern[j]
-
-                # If this cell in the matrix is currently set to zero,
-                # i.e. this pair of varieties (i and j) are unresolved,
-                # and their genotypes are valid and different, then we can
-                # set this cell in the test matrix to 1 (= resolved) -
-                # otherwise it remains set to zero.
-                score += (jchar != "x" and ichar != jchar and
-                          matrix[i, j] == 0)
-
-    return score
+        if x == "x":
+            patterns[i, j] = -1
+        else:
+            patterns[i, j] = x
 
 
-@numba.jit(nopython=True, fastmath=True, nogil=True)
+@numba.jit(nopython=True, nogil=True, fastmath=True, parallel=True)
 def score_patterns(patterns, matrix):
-    best_score = 0
-    best_pattern = None
 
-    npatterns = len(patterns)
+    npatterns: int = patterns.shape[0]
+    ncols: int = patterns.shape[1]
+
+    scores = np.zeros(npatterns, np.int32)
+
+    for p in numba.prange(0, npatterns):
+        score: int = 0
+
+        for i in range(0, ncols):
+            ival: int = patterns[p, i]
+
+            if ival != -1:
+                for j in range(i+1, ncols):
+                    jval: int = patterns[p, j]
+
+                    score += (jval != -1 and ival != jval and
+                              matrix[i, j] == 0)
+
+        scores[p] = score
+
+    best_score: int = 0
+    best_pattern: int = 0
 
     for i in range(0, npatterns):
-        pattern = patterns[i]
-        score = score_pattern(pattern, matrix)
-
-        # If the current marker is better than others tested, it becomes the
-        # new bestscore and its matrix becomes the one to beat!
-        if score > best_score:
-            best_score = score
-            best_pattern = pattern
+        if scores[i] > best_score:
+            best_score = scores[i]
+            best_pattern = i
 
     return (best_score, best_pattern)
 
 
+@numba.jit(nopython=True, fastmath=True, nogil=True, parallel=True)
 def create_matrix(pattern, matrix):
     n = len(pattern)
 
     m = np.zeros((n, n), np.int8)
 
-    for i in range(0, n):
-        ichar = pattern[i]
+    for i in numba.prange(0, n):
+        ival = pattern[i]
 
-        if ichar != "x":
+        if ival != -1:
             for j in range(i+1, n):
-                jchar = pattern[j]
+                jval = pattern[j]
 
                 # If this cell in the matrix is currently set to zero,
                 # i.e. this pair of varieties (i and j) are unresolved,
                 # and their genotypes are valid and different, then we can
                 # set this cell in the test matrix to 1 (= resolved) -
                 # otherwise it remains set to zero.
-                if jchar != "x" and ichar != jchar and matrix[i, j] == 0:
+                if jval != -1 and ival != jval and matrix[i, j] == 0:
                     m[i, j] = 1
 
     return m
 
 
-n = len(selected)
-
-print(f"{n} distinct SNP patterns selected for constructing the optimal "
-      "dataset")
+print(f"{len(selected)} distinct SNP patterns selected for "
+      "constructing the optimal dataset")
 
 # create the scoring matrix
+ncols: int = len(selected[0])
 matrix = np.zeros((ncols, ncols), np.int8)
 
 print(f"Built initial scoring matrix: {matrix.shape}")
 
-cumulative = 0
-target_size = int((ncols * (ncols-1)) / 2)
+perfect_score: int = int((ncols * (ncols-1)) / 2)
 
-print(f"{target_size} varietal comparisons")
-
-current_score = 1
+print(f"{perfect_score} varietal comparisons")
 
 print("\nIteration\tCumulativeResolved\tProportion\tMarkerID\tPattern")
-
-iteration = 0
-
 
 # This is the main loop where we iterate through all of the available rows of
 # SNP data and find the one that adds the most new "1s" to the overall
 # scoring matrix
 # This loop will exit when the current_score value is zero - i.e. adding
 # another row doesn't add anything to the overall matrix score
+iteration: int = 0
+cumulative_score: int = 0
+current_score: int = 1
+
 while current_score > 0:
     # This hash is the current working score matrix - it will be evaluated
     # for this iteration and its contents added to the overal matrix
     # once we have decided which SNP row is best for this iteration
-    best_matrix = np.zeros((ncols, ncols), np.int8)
-    best_score = 0
     iteration += 1
 
-    (best_score, best_pattern) = score_patterns(selected, matrix)
+    (best_score, best_pattern) = score_patterns(patterns, matrix)
 
-    idx = pattern_to_idx[best_pattern]
-
-    print(f"best score is {best_score}")
+    idx = pattern_to_idx[selected[best_pattern]]
 
     if best_score > 0:
-        cumulative += best_score
-        proportion_resolved = cumulative / target_size
-        resolved = "%.6f" % proportion_resolved
+        cumulative_score += best_score
+        proportion_resolved = cumulative_score / perfect_score
+        matrix += create_matrix(patterns[best_pattern], matrix)
 
-        print(f"{iteration}\t{cumulative}\t{resolved}\t{idx}\t"
-              f"{best_pattern}")
+        print(f"{iteration}\t{cumulative_score}\t"
+              f"{proportion_resolved:.6f}\t{idx}\t"
+              f"{selected[best_pattern]}")
 
-    current_score = best_score
-
-    matrix += create_matrix(best_pattern, matrix)
-
-    best_score = 0.0
-    best_matrix = None
+    current_score: int = best_score
